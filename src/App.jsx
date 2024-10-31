02993111194f5cde93b9a195ee9f5277442dea9d
durjoydutta001 <durjoydutta001@gmail.com>
2024-10-31 20:28:56 +0000

    Add and Delete functionalities using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-// import { useState } from 'react'
-import AddTask from './components/AddTask'
+import { useState ,useRef} from 'react'
+import AddTaskForm from './components/AddTaskForm'
 import Header from './components/Header'
 import TaskCard from './components/TaskCard'
 import Footer from './components/Footer'
 
 const App = () => {
-  // const [toDoList, setToDoList] = useState([]);
-  // const newTaskRef = useRef(null);
+  const [toDoList, setToDoList] = useState([]);
+  const newTaskRef = useRef(null);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const newTask = newTaskRef.current.value;
+    if (newTask === "") {
+      alert("The task entry is empty!");
+      return;
+    }
+    addTask(newTask);
+  }
+
+  const addTask = (newTask) => {
+    const newList = [...toDoList, {
+      id: crypto.randomUUID(),
+      task: newTask,
+      completed: false
+    }];
+    setToDoList(newList);
+    console.log(newList);
+    newTaskRef.current.value = "";
+  }
+
+  const deleteTask = (id) => {
+    const newList = toDoList.filter((item) => item.id !== id);
+    setToDoList(newList);
+    console.log(newList)
+  }
 
   return (
     <>
@@ -14,14 +41,14 @@ const App = () => {
       
       <div className="topbar w-full flex flex-col flex-nowrap justify-evenly mt-4 gap-10">
         <Header/>
-        <AddTask/>
+        <AddTaskForm handleClick = {handleClick} newTaskRef = {newTaskRef}/>
         <div className="divider w-full h-1 bg-gray-600"></div>
       </div>
 
       <div className="main w-full flex flex-col grow gap-5">
-        <TaskCard/>
-        <TaskCard/>
-        <TaskCard/>
+        {toDoList.map(({task, id})=> {
+          return <TaskCard task = {task} id = {id} key = {id} deleteTask = {deleteTask}/>
+        })}
       </div>
 
       <Footer/>
