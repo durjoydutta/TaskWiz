d243852712db4919893abafdbc76cc33fe6a8921
durjoydutta001 <durjoydutta001@gmail.com>
2024-11-01 14:23:34 +0000

    fix error in signin refresh

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,7 +19,6 @@ const login = () => {
       const user = result.user;
       // ... (Your code to handle successful sign-in) ...
       console.log("Sign-in successful:", user);
-      window.location.reload();
       navigate("/");
 
     } catch (error) {
